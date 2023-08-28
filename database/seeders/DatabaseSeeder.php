da7f9d19fc2ccd780af4366d1ca9e2d22d4f3033
syaoki09 <syaokifaradisa09@gmail.com>
2023-08-28 13:06:24 +0800

    feat: Setup spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,23 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@moonton.test',
+            'password' => bcrypt("password")
+        ]);
+
+        $user->assignRole('admin');
     }
 }
