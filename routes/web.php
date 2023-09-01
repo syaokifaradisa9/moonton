812d2ec991ec3ebbad5c028cdf00ef193e931acd
syaoki09 <syaokifaradisa09@gmail.com>
2023-09-01 20:27:04 +0800

    feat: integration detail movie

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\User\DashboardController;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\MovieController;
+use App\Http\Controllers\User\DashboardController;
 
 Route::redirect('/', '/login');
 
-Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard')->group(function(){
+Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function(){
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
 });
 
 Route::middleware('auth')->group(function () {
